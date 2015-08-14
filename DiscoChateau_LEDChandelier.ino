195958b0ecfad621e13fe73621e6795252ea38d3
Louisa Laroche <laroche726@gmail.com>
2015-08-13 20:32:48 -0700

    Inital LED code Commit: Includes confetti, unfinished startup and sinelon animations

--- DiscoChateau_LEDChandelier.ino
+++ DiscoChateau_LEDChandelier.ino
@@ -1,21 +1,27 @@
 #include <FastLED.h>
+FASTLED_USING_NAMESPACE
+
+#if FASTLED_VERSION < 3001000
+#error "Requires FastLED 3.1 or later; check github for latest code."
+#endif
 
 //#define LED_PIN     5
 #define NUM_LEDS    150
 #define BRIGHTNESS  200
-#define LED_TYPE    WS2811
+#define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
-#define duration 10
+#define FRAMES_PER_SECOND 10
+
 
 //Test LED Strip
-//CRGB leds[NUM_LEDS];
+CRGB led5[NUM_LEDS];
 
 //The Full 13 Strips Listed Below
 CRGB led1[NUM_LEDS];
 CRGB led2[NUM_LEDS];
 CRGB led3[NUM_LEDS];
 CRGB led4[NUM_LEDS];
-CRGB led5[NUM_LEDS];
+//CRGB led5[NUM_LEDS];
 CRGB led6[NUM_LEDS];
 CRGB led7[NUM_LEDS];
 CRGB led8[NUM_LEDS];
@@ -28,10 +34,11 @@ CRGB led13[NUM_LEDS];
 /*  
 *  List of pins used: number of pin will correspond to
 *  led strip number. Except for led1 which will be using
-*  the analog pin A1
+*  digital bin 14
 */ 
+#define LED_PIN5 5
 
-#define LED_PIN1 A1
+#define LED_PIN1 14
 #define LED_PIN2 2
 #define LED_PIN3 3
 #define LED_PIN4 4
@@ -55,22 +62,24 @@ void setup() {
   delay( 3000 ); // power-up safety delay
   
   //Test LED Strip Setup
-  //FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<LED_TYPE, LED_PIN5, COLOR_ORDER>(led5, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  
+  Serial.begin(115200); 
   
   //Full 13 LED Setup
-  FastLED.addLeds<LED_TYPE, LED_PIN1, COLOR_ORDER>(led5, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.addLeds<LED_TYPE, LED_PIN2, COLOR_ORDER>(led5, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.addLeds<LED_TYPE, LED_PIN3, COLOR_ORDER>(led5, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.addLeds<LED_TYPE, LED_PIN4, COLOR_ORDER>(led5, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<LED_TYPE, LED_PIN1, COLOR_ORDER>(led1, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<LED_TYPE, LED_PIN2, COLOR_ORDER>(led2, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<LED_TYPE, LED_PIN3, COLOR_ORDER>(led3, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<LED_TYPE, LED_PIN4, COLOR_ORDER>(led4, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.addLeds<LED_TYPE, LED_PIN5, COLOR_ORDER>(led5, NUM_LEDS).setCorrection( TypicalLEDStrip );
- /* FastLED.addLeds<LED_TYPE, LED_PIN6, COLOR_ORDER>(led5, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.addLeds<LED_TYPE, LED_PIN7, COLOR_ORDER>(led5, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.addLeds<LED_TYPE, LED_PIN8, COLOR_ORDER>(led5, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.addLeds<LED_TYPE, LED_PIN9, COLOR_ORDER>(led5, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<LED_TYPE, LED_PIN6, COLOR_ORDER>(led6, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<LED_TYPE, LED_PIN7, COLOR_ORDER>(led7, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<LED_TYPE, LED_PIN8, COLOR_ORDER>(led8, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<LED_TYPE, LED_PIN9, COLOR_ORDER>(led9, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.addLeds<LED_TYPE, LED_PIN10, COLOR_ORDER>(led5, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.addLeds<LED_TYPE, LED_PIN11, COLOR_ORDER>(led5, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.addLeds<LED_TYPE, LED_PIN12, COLOR_ORDER>(led5, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.addLeds<LED_TYPE, LED_PIN13, COLOR_ORDER>(led5, NUM_LEDS).setCorrection( TypicalLEDStrip );*/
+  FastLED.addLeds<LED_TYPE, LED_PIN13, COLOR_ORDER>(led5, NUM_LEDS).setCorrection( TypicalLEDStrip );
   
   
 }
@@ -78,80 +87,100 @@ void setup() {
 
 void loop()
 {
-     startUpSequence(CRGB::Blue);
+   /* 
+   *  There are three diffeent animations below that can be used seperately
+   *  To view each one uncomment thier corresponding lines
+   *  Warning: Due to high number of leds set up the animations may  
+   */
+  
+   //Uncomment next three lines for Confetti animation example on led 2 and 5:
+   confetti(led5);
+   confetti(led2);
+   FastLED.show(); 
+   
+   //Uncomment for start up Sequence
+   //startUpSequence(CRGB::Purple, led5);  
+     
+   //Uncomment for light running sequence
+    /*sinelon(led5); 
+    sinelon(led2);
+    FastLED.show(); */
 }
 
-void startUpSequence(CRGB color){
+void startUpSequence(CRGB color, CRGB led[]){
   
    uint8_t startIndex = 0;
-   float count = 0; 
    fill_solid(currentPalette, 16, color);
-   FillLEDsFromPaletteColors(startIndex);
-   
-   while (true){
-     FastLED.setBrightness(abs(sin(count))*BRIGHTNESS);
-     count+=.01;
+   FillLEDsFromPaletteColors(startIndex, led);
+    
+   for (float i = 0; i < PI*2; i+=.01){  
+     FastLED.setBrightness(abs(sin(i))*BRIGHTNESS);
      FastLED.show();
-     //FastLED.delay(duration);
    }
-   
-}
+  }
 //From FASTLED Base Function to fill the LED Strip 
-void FillLEDsFromPaletteColors( uint8_t colorIndex)
+void FillLEDsFromPaletteColors( uint8_t colorIndex, CRGB led[])
 {
-  uint8_t brightness = 155;
+  uint8_t brightness = 200;
   
   for( int i = 0; i < NUM_LEDS; i++) {
-    led5[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
+    led[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
     colorIndex += 1;
   }
 }
 
-//Blend the Colors Closer together - in Progress
-void FillLEDsFromPaletteColors( uint8_t colorIndex, uint8_t startLed, uint8_t endLed )
-{
-  uint8_t brightness = 255;
-  
-  for( startLed ; startLed < endLed; startLed++) { 
-    led5[startLed] = ColorFromPalette( currentPalette, colorIndex, brightness,   currentBlending);
-    colorIndex += 3;
-  }
-}
 /*
  * Animate Light Mouse Running through the LEDs
+ * Old Implementation: Do not Use
 */
-void runningMouseV2(){
+void runningMouseV2(CRGB led[]){
   currentPalette = whiteMouseRunning_p;
-  currentBlending = BLEND; 
+  currentBlending = LINEARBLEND; 
   
   static uint8_t startIndex = 0;
   startIndex = startIndex + 1; /* motion speed */
-  FillLEDsFromPaletteColors(startIndex);
-  
-  FastLED.show();
-  FastLED.delay(duration);
+  FillLEDsFromPaletteColors(startIndex, led);
+  FastLED.delay(FRAMES_PER_SECOND);
 }
 
+// Run through the Led Strip
+void sinelon(CRGB led[])
+{
+  // a colored dot sweeping back and forth, with fading trails
+  fadeToBlackBy( led, NUM_LEDS, 20);
+  int pos = beat8(2)/1.7;
+  led[pos] += CHSV( 192, 192, 192);
+}
+void confetti(CRGB led[]) 
+{
+  // random colored speckles that blink in and fade smoothly
+  fadeToBlackBy( led, NUM_LEDS, 10);
+  int pos = random16(NUM_LEDS);
+  led[pos] += CHSV( 134, 200, 255);
+}
 const TProgmemPalette16 whiteMouseRunning_p PROGMEM =
 {
-  CRGB::White,
-  CRGB::Black, 
-  CRGB::Black,
-  CRGB::Black,
-
   CRGB::Black,
   CRGB::Black,
   CRGB::Black,
   CRGB::Black,
-
+  
   CRGB::Black,
   CRGB::Black,
   CRGB::Black,
   CRGB::Black,
+  
   CRGB::Black,
   CRGB::Black,
   CRGB::Black,
-  CRGB::Black
+  CRGB::Black,
+  
+  CRGB::Black,
+  CRGB::Black,
+  CRGB::Black,
+  CRGB::White
 };
+
+
 
 
